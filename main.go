c671f08e746229f36d1d568312a3d951f42ba57c
Pablo Zuta <pablo.zuta.h@gmail.com>
2023-01-06 04:36:33 -0400

    server usando go first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Book struct{
+	Title string `json:"title"`
+	Author string `json:"author"`
+	Pages int `json:"pages"`
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	w.Write([]byte("<h1 style='color: steelblue'>Hola Go</h1>"))
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	book := Book { Title: "The Gunslinger", Author:  "Stephen King", Pages: 304}
+
+	json.NewEncoder(w).Encode(book)
+}
+
+func main() {
+	http.HandleFunc("/hola", Hello)
+	http.HandleFunc("/book", GetBook)
+
+	log.Fatal(http.ListenAndServe(":5100", nil))
+
+}
